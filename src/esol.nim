e1b335cb45cdf056a647bd1bbfaaaf04961795a2
LeBakaSable <hugogole.ito@gmail.com>
2024-04-25 12:18:19 +0200

    feat: for loop & seq definition

--- src/esol.nim
+++ src/esol.nim
@@ -8,61 +8,12 @@ import
   fusion/matching,
   std/streams,
   std/enumerate,
-  std/os
+  std/os,
+  std/tables
     
 template panic(args: varargs[string, `$`]) =
   error(args)
   quit(1)
-
-type
-  Symbol = object
-    name: string
-  Step = enum
-    Left, Right
-  Case = object
-    state: Symbol
-    read: Symbol
-    write: Symbol
-    step: Step
-    next: Symbol
-  Machine = object
-    state: Symbol
-    tape: seq[Symbol]
-    tape_default: Symbol
-    head: int
-    halt: bool
-  # TODO: proper lexer so symbols have a location
-  Lexer = seq[string]
-
-method next(self: var Machine, cases: seq[Case]) {.base.} =
-  for `case` in cases:
-    if `case`.state == self.state and `case`.read == self.tape[self.head]:
-      self.tape[self.head] = `case`.write
-      case `case`.step:
-        of Left:
-          if self.head == 0:
-            panic "Tape underflow."
-          self.head -= 1
-        of Right:
-          self.head += 1
-          if self.head >= self.tape.len:
-            self.tape.add(self.tape_default)
-      self.state = `case`.next
-      self.halt = false
-      break
-  
-method print(self: Machine) {.base.} =
-  let buffer = new_string_stream()
-  buffer.write self.state.name, ": "
-  var head = 0
-  for i, symbol in enumerate(self.tape):
-    if i == self.head:
-      head = buffer.data.len
-    buffer.write symbol.name, " "
-  echo buffer.data
-  for _ in 0..<head:
-    stdout.write " "
-  echo "^"
   
 proc peek[T](s: seq[T]): Option[T] =
   if s.len > 0:
@@ -76,37 +27,190 @@ proc next[T](s: var seq[T]): Option[T] =
   if s.len > 0:
     result = some(s[0])
     s = s[1..s.len-1]
+
+type
+  Symbol = object
+    name: string
+  StatementKind = enum
+    CaseStatement
+    ForStatement
+  Statement = ref object
+    case kind: StatementKind
+    of CaseStatement:
+      state: Symbol
+      read: Symbol
+      write: Symbol
+      step: Symbol
+      next: Symbol
+    of ForStatement:
+      `var`: Symbol
+      seq: Symbol
+      body: Statement
+  Program = object
+    statements: seq[Statement]
+    seqs: Table[string, seq[Symbol]]
+  Machine = object
+    state: Symbol
+    tape: seq[Symbol]
+    tape_default: Symbol
+    head: int
+    halt: bool
+  # TODO: proper lexer so symbols have a location
+  Lexer = seq[string]
+  
+proc substitute(self: Statement, `var`: Symbol, symbol: Symbol): Statement =
+  case self.kind
+  of CaseStatement:
+    result = Statement(
+      kind: CaseStatement,
+      state: if self.state == `var`: symbol else: self.state,
+      read:  if self.read  == `var`: symbol else: self.read,
+      write: if self.write == `var`: symbol else: self.write,
+      # TODO: substitute step
+      step: self.step,
+      next:  if self.next  == `var`: symbol else: self.next,
+    )
+  of ForStatement:
+    return Statement(
+      kind: ForStatement,
+      `var`: self.`var`,
+      seq: if self.seq == `var`: symbol else: self.seq,
+      body: self.body.substitute(self.`var`, self.seq),
+    )
+
+proc entry_state(self: Statement, program: Program): Option[Symbol] =
+  case self.kind
+  of CaseStatement: return some(self.state)
+  of ForStatement:
+    if program.seqs.contains(self.seq.name):
+      if Some(@symbol) ?= program.seqs[self.seq.name].peek:
+        return self.body.substitute(self.`var`, symbol).entry_state(program)
+    else:
+      panic "Unknown sequence `", self.seq.name, "`."
+  
+proc match(self: Statement, program: Program, state: Symbol, read: Symbol): Option[(Symbol, Symbol, Symbol)] =
+  case self.kind
+  of CaseStatement:
+    if self.state == state and self.read == read:
+      return some((self.write, self.step, self.next))
+  of ForStatement:
+    if program.seqs.contains(self.seq.name):
+      for symbol in program.seqs[self.seq.name]:
+        if Some(@triple) ?= self.body.substitute(self.`var`, symbol).match(program, state, read):
+          return some(triple)
+    else:
+      panic "Unknown sequence `", self.seq.name, "`."
+
+proc entry_state(self: Program): Option[Symbol] =
+  for statement in self.statements:
+    if Some(@state) ?= statement.entry_state(self):
+      return some(state)
+
+proc next(self: var Machine, program: Program) =
+  for statement in program.statements:
+    if Some(@matched) ?= statement.match(program, self.state, self.tape[self.head]):
+      let (write, step, next) = matched
+      self.tape[self.head] = write
+      case step.name:
+        of "<-":
+          if self.head == 0:
+            panic "Tape underflow."
+          self.head -= 1
+        of "->":
+          self.head += 1
+          if self.head >= self.tape.len:
+            self.tape.add(self.tape_default)
+      self.state = next
+      self.halt = false
+      break
+
+proc print(self: Machine) =
+  let buffer = new_string_stream()
+  buffer.write self.state.name, ": "
+  var head = 0
+  for i, symbol in enumerate(self.tape):
+    if i == self.head:
+      head = buffer.data.len
+    buffer.write symbol.name, " "
+  echo buffer.data
+  for _ in 0..<head:
+    stdout.write " "
+  echo "^"
   
 proc parse_symbol(lexer: var Lexer): Symbol =
-  case lexer.next
-  of Some(@name): return Symbol(name: name)
-  of None(): panic "Expected symbol but reached the end of the input."
+  if Some(@name) ?= lexer.next:
+    return Symbol(name: name)
+  else:
+    panic "Expected symbol but reached the end of the input."
 
-proc parse_step(lexer: var Lexer): Step =
+proc expect_symbol(lexer: var Lexer, expected_names: varargs[string]): Symbol =
   let symbol = parse_symbol(lexer)
-  case symbol.name
-  of "->": return Right
-  of "<-": return Left
-  else: panic "Expected arrow but got `", symbol.name, "`."
-  
-proc parse_case(lexer: var Lexer): Case =
-  Case(
+  for name in expected_names:
+    if symbol.name == name:
+      return symbol
+  var buffer = new_string_stream()
+  for i, name in enumerate(expected_names):
+    if i == 0:
+      buffer.write "`", name, "`"
+    elif i + 1 == expected_names.len:
+      buffer.write ", or `", name, "`"
+    else:
+      buffer.write ", `", name, "`"
+  panic "Expected ", buffer.data, " but got `", symbol.name, "`."
+
+proc parse_seq(lexer: var Lexer): seq[Symbol] =
+  discard expect_symbol(lexer, "{")
+  while lexer.peek.is_some:
+    let symbol = parse_symbol(lexer)
+    if symbol.name == "}": break
+    result.add(symbol)
+
+proc parse_case(lexer: var Lexer): Statement =
+  Statement(
+    kind: CaseStatement,
     state: parse_symbol(lexer),
     read: parse_symbol(lexer),
     write: parse_symbol(lexer),
-    step: parse_step(lexer),
-    next: parse_symbol(lexer),
+    step: expect_symbol(lexer, "->", "<-"),
+    next: parse_symbol(lexer)  ,
   )
+  
+proc parse_statement(lexer: var Lexer): Statement =
+  let key = expect_symbol(lexer, "case", "for")
+  case key.name
+  of "case": return parse_case(lexer)
+  of "for":
+    let `var` = parse_symbol(lexer)
+    discard expect_symbol(lexer, "in")
+    let seq = parse_symbol(lexer)
+    let body = parse_statement(lexer)
+    return Statement(kind: ForStatement, `var`: `var`, seq: seq, body: body)
+    
+proc parse_program(lexer: var Lexer): Program =
+  while Some(@key) ?= lexer.peek:
+    case key
+    of "case", "for":
+      result.statements.add(parse_statement(lexer))
+    of "let":
+      discard lexer.next
+      let name = parse_symbol(lexer)
+      if result.seqs.has_key(name.name):
+        panic "Redefinition of sequence `", name.name, "`."
+      let seq = parse_seq(lexer)
+      result.seqs[name.name] = seq
+    else:
+      panic "Unknown keyword `", key, "`."
+
+proc parse_program_file(program_path: string): (Program, string) =
+  let program_source = try: read_file(program_path)
+                       except: panic "Could not read file `", program_path, "`."
+
+  var lexer = program_source.split.filter_it(it != "")
+  return (parse_program(lexer), program_source)
 
 proc parse_symbols(lexer: var Lexer): seq[Symbol] =
-  result = new_seq[Symbol]()
   while lexer.peek.is_some:
     result.add(parse_symbol(lexer))
-
-proc parse_cases(lexer: var Lexer): seq[Case] =
-  result = new_seq[Case]()
-  while lexer.peek.is_some:
-    result.add(parse_case(lexer))
 
 proc parse_tape_file(tape_path: string): (seq[Symbol], string) =
   let tape_source = try: read_file(tape_path)
@@ -115,50 +219,41 @@ proc parse_tape_file(tape_path: string): (seq[Symbol], string) =
   var lexer = tape_source.split.filter_it(it != "")
   return (parse_symbols(lexer), tape_source)
 
-proc parse_cases_file(cases_path: string): (seq[Case], string) =
-  let cases_source = try: read_file(cases_path)
-                     except: panic "Could not read file `", cases_path, "`."
-
-  var lexer = cases_source.split.filter_it(it != "")
-  return (parse_cases(lexer), cases_source)
-
-proc usage(program: string) =
-  stderr.write_line "Usage: ", program, " <input.esol> <input.tape>"
-    
+proc usage(app_file: string) =
+  stderr.write_line "Usage: ", app_file, " <input.esol> <input.tape>"
+  
 proc main() =
   var logger = new_console_logger()
   add_handler(logger)
 
   var args = command_line_params()
-  let program = get_app_filename()
+  let app_file = get_app_filename()
 
-  var cases_path: string
-  case args.next
-  of Some(@path): cases_path = path
-  of None():
-    usage(program)
-    panic "No cases file is provided."
+  var program_path: string
+  if Some(@path) ?= args.next:
+    program_path = path
+  else:
+    usage(app_file)
+    panic "No program file is provided."
 
   var tape_path: string
-  case args.next
-  of Some(@path): tape_path = path
-  of None():
-    usage(program)
+  if Some(@path) ?= args.next:
+    tape_path = path
+  else:
+    usage(app_file)
     panic "No tape file is provided."
 
-  let (cases, cases_source) = parse_cases_file(cases_path)
-  
+  let (program, _) = parse_program_file(program_path)
+
   var state: Symbol
-  case cases.peek
-  of Some(@c): state = c.state
-  of None(): panic "Cases file must contain at least one case."
+  if Some(@s) ?= program.entry_state: state = s
+  else: panic "Source file must contain at least one case."
   
-  let (tape, tape_source) = parse_tape_file(tape_path)
+  let (tape, _) = parse_tape_file(tape_path)
 
   var tape_default: Symbol
-  case tape.last
-  of Some(@symbol): tape_default = symbol
-  of None(): panic "Tape file should not be empty, it must contain at least one symbol to be repeated indefinitely."
+  if Some(@symbol) ?= tape.last: tape_default = symbol
+  else: panic "Tape file should not be empty, it must contain at least one symbol to be repeated indefinitely."
 
   var machine = Machine(
     state: state,
@@ -171,6 +266,6 @@ proc main() =
   while not machine.halt:
     machine.print
     machine.halt = true
-    machine.next(cases)
+    machine.next(program)
 
 when isMainModule: main()
